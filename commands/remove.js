156e8d110c573944fab5e9bf8d772acab7f54025
unknown <leoczuy@Gmail.com>
2022-09-03 15:01:14 -0300

    att_remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -16,14 +16,22 @@ module.exports = {
       let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
       if (!user) return message.reply("Membro não especificado!")
       let roleC = await user._roles
-      let roles = ['852646292424753213' /*Server Booster*/,
+      let roles = [
+        '852646292424753213' /*Server Booster*/,
         '840093797839798322' /*CTT*/,
         '840105438455922729' /*Cinema*/,
         '979947281325363250' /*Follow*/,
         '914696516990550047' /*Eventos*/,
         '914696715771203584' /*Amigo Secreto 2021*/,
         '934440613032648724' /*BossPartyS14*/,
-        '849727477265334312' /*Language*/,]
+        '849727477265334312' /*Language*/,
+        '833102112186826843' /*Roles*/
+      ]
+
+      const update = await report.findOneAndUpdate(
+        { discordId: user.id },
+        { atividade: 0 }
+      )
       let c = 0
       const supportGuild = client.guilds.cache.get('833725119720325190')
       const userBomb = supportGuild.members.cache.get(user.id)
@@ -36,11 +44,6 @@ module.exports = {
       }
       try { userBomb.kick("Expulso da Guilda") } catch (error) { }
       user.setNickname("")
-      const update = await report.findOneAndUpdate(
-        { discordId: user.id },
-        { atividade: 0 }
-      )
-      await sleep(500);
       message.reply(`Cargos removidos de ${user}.`)
     }
 
