fbe3459dd6325e0a2bc4d20cdfcb74990e7ed15c
unknown <leoczuy@Gmail.com>
2022-09-03 14:32:11 -0300

    atualização_no_cadastrar.js

--- commands/cadastrar.js
+++ commands/cadastrar.js
@@ -6,6 +6,8 @@ module.exports = {
     name: "cadastrar",
 
     run: async(client, message, args) => {
+
+
         if(message.guildId != "831483672065736704") return;
         if(!message.member.permissions.has("MANAGE_ROLES")) {
             message.reply("Você não tem permissão!")
@@ -15,6 +17,19 @@ module.exports = {
             if(!user) return message.reply("Membro não especificado! k!cadastrar <@Usuario> <Nickname>")
             if(!nickname) return message.reply("Nickname não especificado! k!cadastrar <@Usuario> <Nickname>")
             let dados = await report?.find({discordId: user.id}).exec()
+            
+            function adicionarRole(){
+                user.setNickname(`VD | ${nickname}`)
+                setTimeout(function(){ user.roles.add("832369903951675473") }, 500)
+                setTimeout(function(){ user.roles.remove("832425621707685938") }, 500)
+                setTimeout(function(){ user.roles.add("833102112186826843") }, 500)
+                setTimeout(function(){ user.roles.add("979947281325363250") }, 500)
+                setTimeout(function(){ user.roles.add("914696516990550047") }, 500)
+                setTimeout(function(){ user.roles.add("849727477265334312") }, 500)
+                message.reply(`Usuário <@${user.id}> cadastrado.`)
+
+            }
+
             if(dados == false) {
                 const cadastro = await report.create({
                     nickname: nickname,
@@ -22,30 +37,16 @@ module.exports = {
                     atividade: 1,
                     report: ""
                 })
-                user.setNickname(`VD | ${nickname}`)
-                user.roles.add("832369903951675473")
-                user.roles.remove("832425621707685938")
-                user.roles.add("833102112186826843")
-                user.roles.add("840093797839798322")
-                user.roles.add("979947281325363250")
-                user.roles.add("914696516990550047")
-                user.roles.add("849727477265334312")
-                message.reply(`Usuário <@${user.id}> cadastrado.`)
+                adicionarRole()
+
+
             } else {
                 const  update = await report.findOneAndUpdate(
                     {discordId: user.id},
                     {nickname: nickname,
                     atividade: 1}
                 )
-                user.setNickname(`VD | ${nickname}`)
-                user.roles.add("832369903951675473")
-                user.roles.remove("832425621707685938")
-                user.roles.add("833102112186826843")
-                user.roles.add("840093797839798322")
-                user.roles.add("979947281325363250")
-                user.roles.add("914696516990550047")
-                user.roles.add("849727477265334312")
-                message.reply(`Usuário <@${user.id}> na ativa novamente!`)
+                adicionarRole()
             }
 
 
--- commands/removewarn.js
+++ commands/removewarn.js
@@ -26,7 +26,6 @@ module.exports = {
                     {report: reportArray}
                 )
                 message.reply("Warn removido!")
-                //teste
               
             }
         }
