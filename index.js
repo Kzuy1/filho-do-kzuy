c15d1ad21f6460a3b5b6d45325c671f6e643fe94
unknown <leoczuy@Gmail.com>
2022-10-23 22:14:22 -0300

    v3.3.11

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//DISCORD.JS
 const { Client, Intents } = require('discord.js');
 const { connect, default: mongoose } = require('mongoose');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING, Intents.FLAGS.GUILD_MEMBERS] })
@@ -7,25 +6,11 @@ const fs = require('fs')
 const eventosLista = require("./models/eventosLista.js")
 const report = require("./models/reportModel.js")
 
-
-
-//BOT INICIADO
 client.on("ready", () => {
   console.log("Estou Online!")
 })
 
-client.on("raw", async dados => {
-  if (dados.t !== "CHANNEL_CREATE" && dados.t !== "MESSAGE_CREATE") return
 
-  try {
-    const commandReactions = require(`./reactions/recruitment.js`)
-    commandReactions.run(client, dados)
-  } catch (err) {
-    console.log(err)
-  }
-})
-
-//REAÇÕES POR EMOJI
 client.on("raw", async dados => {
   if (dados.t !== "MESSAGE_REACTION_ADD" && dados.t !== "MESSAGE_REACTION_REMOVE") return
   if (dados.d.message_id === "1008456938129981491") {
