aca3a22ba101239cc02384ca816db345fbedf761
unknown <leoczuy@gmail.com>
2023-01-12 20:49:59 -0300

    v3.7.2

--- commands/remove.js
+++ commands/remove.js
@@ -6,54 +6,35 @@ module.exports = {
   name: "remove",
 
   run: async (client, message, args) => {
-
-    function sleep(ms) {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
-    if (message.guildId != "831483672065736704") return;
+    if(!(message.guildId != "831483672065736704" || message.guildId != "1060614825899728986")) return
     if (!message.member.permissions.has(Discord.PermissionFlagsBits.ManageRoles)) {
       message.reply("Você não tem permissão!")
     } else {
       let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
       if (!user) return message.reply("Membro não especificado!")
-      let roleC = await user._roles
-      let roles = [
-        '852646292424753213' /*Server Booster*/,
-        '840093797839798322' /*CTT*/,
-        '840105438455922729' /*Cinema*/,
-        '979947281325363250' /*Follow*/,
-        '914696516990550047' /*Eventos*/,
-        '914696715771203584' /*Amigo Secreto 2021*/,
-        '934440613032648724' /*BossPartyS14*/,
-        '849727477265334312' /*Language*/,
-        '833102112186826843' /*Roles*/
-      ]
 
       const update = await report.findOneAndUpdate(
         { discordId: user.id },
         { atividade: 0 }
       )
       
-      let supportGuild, userBomb, supportGuild2, userBomb2
+      let discordVoodoo, discordAcademy, discordBomb1, discordBomb2
+      let userVoodoo, userAcademy, userBomb1, userBomb2
+      try {discordVoodoo = await client.guilds.cache.get('831483672065736704')} catch (err) {}
+      try {userVoodoo = await discordVoodoo.members.fetch(user.id)} catch (err) {}
+      try {discordAcademy = await client.guilds.cache.get('1060614825899728986')} catch (err) {}
+      try {userAcademy = await discordAcademy.members.fetch(user.id)} catch (err) {}
+      try {discordBomb1 = await client.guilds.cache.get('833725119720325190')} catch (err) {}
+      try {userBomb1 = await discordBomb1.members.fetch(user.id)} catch (err) {}
+      try {discordBomb2 = await client.guilds.cache.get('941706531630633001')} catch (err) {}
+      try {userBomb2 = await discordBomb2.members.fetch(user.id)} catch (err) {}
       
-      try {supportGuild = await client.guilds.cache.get('833725119720325190')} catch (err) {}
-      try {userBomb = await supportGuild.members.fetch(user.id)} catch (err) {}
-      try {supportGuild2 = await client.guilds.cache.get('941706531630633001')} catch (err) {}
-      try {userBomb2 = await supportGuild2.members.fetch(user.id)} catch (err) {}
-      
-      try { userBomb.kick("Expulso da Guilda") } catch (err) {}
+      try { userVoodoo.kick("Expulso da Guilda") } catch (err) {}
+      try { userAcademy.kick("Expulso da Guilda") } catch (err) {}
+      try { userBomb1.kick("Expulso da Guilda") } catch (err) {}
       try { userBomb2.kick("Expulso da Guilda") } catch (err) {}
 
-      for(i = 0; i < roleC.length; i++){
-        if (!roles.includes(roleC[i])) {
-          user.roles.remove(roleC[i])
-          await sleep(500);
-        }
-      }
-
-      user.setNickname("")
-      message.reply(`Cargos removidos de ${user}.`)
+      message.reply(`Expurgo de ${user} realizado com sucesso!.`)
     }
 
   }
--- index.js
+++ index.js
@@ -49,6 +49,19 @@ client.on("guildMemberRemove", async guildRemove => {
   }
 })
 
+client.on("guildMemberRemove", async guildRemove => {
+  if (guildRemove.guild.id != "1060614825899728986") return;
+  let role = guildRemove._roles
+  if (role.includes('1060614826025558040')) {
+    const update = await report.findOneAndUpdate(
+      { discordId: guildRemove.id },
+      { atividade: 0 }
+    )
+    guildRemove.guild.channels.cache.get("1060614827535499380").send(`O Player <@${guildRemove.id}> ou **${guildRemove.nickname}** saiu do Servidor. Não está sendo considerado mais como membro da Guilda.`)
+  }
+})
+
+
 client.on("guildMemberUpdate", async (oldMember, newMember) => {
   let teste1 = oldMember._roles.includes("965732206578393138")
   let teste2 = newMember._roles.includes("965732206578393138")
@@ -60,15 +73,15 @@ client.on("guildMemberUpdate", async (oldMember, newMember) => {
   }
 })
 
-// process.on('unhandRejection', (reason, promise) => {
-//   client.channels.cache.get("1017933097653776484").send(`🚫 Unhandled Promise Rejection:\n\n` + reason, promise)
-// });
-// process.on('uncaughtException', (error, origin) => {
-//   client.channels.cache.get("1017933097653776484").send(`🚫 Uncaught Promise Exception:\n\n` + error, origin)
-// });
-// process.on('uncaughtExceptionMonitor', (error, origin) => {
-//   client.channels.cache.get("1017933097653776484").send(`🚫 Uncaught Promise Exception (Monitor):\n\n` + error, origin)
-// });
+process.on('unhandRejection', (reason, promise) => {
+  client.channels.cache.get("1017933097653776484").send(`🚫 Unhandled Promise Rejection:\n\n` + reason, promise)
+});
+process.on('uncaughtException', (error, origin) => {
+  client.channels.cache.get("1017933097653776484").send(`🚫 Uncaught Promise Exception:\n\n` + error, origin)
+});
+process.on('uncaughtExceptionMonitor', (error, origin) => {
+  client.channels.cache.get("1017933097653776484").send(`🚫 Uncaught Promise Exception (Monitor):\n\n` + error, origin)
+});
 
 async function connectToDatabase() {
   mongoose.set("strictQuery", false)
